768413ccd142c3658383fbd7e2c28871242b98fb
bimus-github <bimus2022@gmail.com>
2024-02-02 16:10:29 +0500

    colors

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,19 +1,19 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
+const tintColorLight = "#2f95dc";
+const tintColorDark = "#fff";
 
 export default {
-  light: {
-    text: '#000',
-    background: '#fff',
-    tint: tintColorLight,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorLight,
+  text: {
+    main: "#ffffff",
+    secondary: "#1B2541",
+    placeholder: "#828282",
+    "expand-more": "#0A44E6",
   },
-  dark: {
-    text: '#fff',
-    background: '#000',
-    tint: tintColorDark,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorDark,
+  bgColor: {
+    main: "#ffffff",
+    card: "#0A44E6",
+    "card-from": "#62B8F6",
+    "card-to": "#2C79C1",
+    cardBg: `linear-gradient(180deg, #62B8F6 0%, #2C79C1 77.96%)`,
+    placeholder: "#F2F2F2",
   },
 };
