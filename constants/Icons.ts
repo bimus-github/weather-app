97c43bfbaf877e7679b54a86d9e86024016a89d1
bimus-github <bimus2022@gmail.com>
2024-02-02 16:10:43 +0500

    icons

---
+++ constants/Icons.ts
@@ -0,0 +1,6 @@
+const plus = require("@/assets/icons/akar-icons_plus.png");
+const menu = require("@/assets/icons/carbon_overflow-menu-vertical.png");
+const heavyRain = require("@/assets/icons/fluent_weather-rain-showers-day-24-regular.png");
+const locationCurrent = require("@/assets/icons/carbon_location-current.png");
+
+export { plus, menu, heavyRain, locationCurrent };
