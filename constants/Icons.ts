cf42b3272b1f6093f593fb69481b38f7e3d32a1f
bimus-github <bimus2022@gmail.com>
2024-02-03 20:14:56 +0500

    fixing

--- constants/Icons.ts
+++ constants/Icons.ts
@@ -2,5 +2,18 @@ const plus = require("@/assets/icons/akar-icons_plus.png");
 const menu = require("@/assets/icons/carbon_overflow-menu-vertical.png");
 const heavyRain = require("@/assets/icons/fluent_weather-rain-showers-day-24-regular.png");
 const locationCurrent = require("@/assets/icons/carbon_location-current.png");
+const backArrow = require("@/assets/icons/bi_arrow-left-short.png");
+const search = require("@/assets/icons/akar-icons_search.png");
+const chechedBox = require("@/assets/icons/Check-Box-Frame-105.png");
+const unChechedBox = require("@/assets/icons/Check-Box-Frame-106.png");
 
-export { plus, menu, heavyRain, locationCurrent };
+export {
+  plus,
+  menu,
+  heavyRain,
+  locationCurrent,
+  backArrow,
+  search,
+  chechedBox,
+  unChechedBox,
+};
