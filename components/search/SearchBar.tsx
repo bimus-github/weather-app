1d01c9b34897562d74910555dd774f6659229a15
bimus-github <bimus2022@gmail.com>
2024-02-04 21:13:11 +0500

    fixing

--- components/search/SearchBar.tsx
+++ components/search/SearchBar.tsx
@@ -1,20 +1,33 @@
-import { View, Text, StyleSheet, TextInput, Image } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import React, { useState } from "react";
 import Colors from "@/constants/Colors";
 import { search } from "@/constants/Icons";
 
 const SearchBar = () => {
+  const [city, setCity] = useState<string>("");
+  const handleSearch = () => {};
   return (
     <View style={styles.bar}>
-      <Image
-        source={search}
-        style={{ width: 16, height: 16, tintColor: "#4f4f4f" }}
-      />
+      <TouchableOpacity onPress={handleSearch}>
+        <Image
+          source={search}
+          style={{ width: 20, height: 20, tintColor: "#4f4f4f" }}
+        />
+      </TouchableOpacity>
       <TextInput
         style={{ flex: 1 }}
         placeholderTextColor={Colors.text.placeholder}
         placeholder="Search Your City"
         keyboardType="twitter"
+        value={city}
+        onChangeText={setCity}
       />
     </View>
   );
@@ -26,7 +39,7 @@ const styles = StyleSheet.create({
   bar: {
     marginVertical: 10,
     width: 326,
-    height: 32,
+    height: 44,
     borderRadius: 8,
     backgroundColor: Colors.bgColor.placeholder,
     paddingHorizontal: 10,
