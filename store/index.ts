d61c8f6b9b80a7f0fbe957f2c0b761afded63c71
bimus-github <bimus2022@gmail.com>
2024-02-05 16:09:08 +0500

    fixing

--- store/index.ts
+++ store/index.ts
@@ -1,10 +1,18 @@
 // src/store/index.ts
 import { configureStore } from "@reduxjs/toolkit";
 import currentLocation from "./slices/currentLocation";
+import currentWeather from "./slices/currentWeather";
+import settings from "./slices/settings";
+import week from "./slices/week";
+import today from "./slices/today";
 
 const store = configureStore({
   reducer: {
     location: currentLocation,
+    current: currentWeather,
+    settings: settings,
+    week: week,
+    today: today,
   },
 });
 
