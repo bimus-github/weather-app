986588a4ccb053fd4c644e83bbfd0274da46e131
bimus-github <bimus2022@gmail.com>
2024-02-05 16:08:08 +0500

    fixing

---
+++ components/home/Item.tsx
@@ -0,0 +1,37 @@
+import { View, Text, Image, ImageSourcePropType } from "react-native";
+import React from "react";
+import { locationCurrent } from "@/constants/Icons";
+import Colors from "@/constants/Colors";
+import { useAppSelector } from "@/store/hooks";
+
+const Item = ({
+  name,
+  value,
+  sourceIcon,
+}: {
+  name?: string;
+  value?: string;
+  sourceIcon?: ImageSourcePropType;
+}) => {
+  return (
+    <View
+      style={{
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 5,
+      }}
+    >
+      <Image
+        source={sourceIcon}
+        style={{ width: 32, height: 32, tintColor: Colors.text.main }}
+      />
+      <View style={{ alignItems: "center" }}>
+        <Text style={{ color: Colors.text.main, fontSize: 12 }}>{value}</Text>
+        <Text style={{ color: Colors.text.main, fontSize: 12 }}>{name}</Text>
+      </View>
+    </View>
+  );
+};
+
+export default Item;
