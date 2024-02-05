55584bb9cf5155d6cb1eeb055b74a5ae4861698c
bimus-github <bimus2022@gmail.com>
2024-02-05 17:33:05 +0500

    fixing

--- components/home/MainAnimatedCard.tsx
+++ components/home/MainAnimatedCard.tsx
@@ -2,7 +2,6 @@ import { View, Text, Animated, Image } from "react-native";
 import React from "react";
 import Dimentions from "@/constants/Dimentions";
 import Colors from "@/constants/Colors";
-import { heavyRain } from "@/constants/Icons";
 import { useAppSelector } from "@/store/hooks";
 
 const MainAnimatedCard = ({
@@ -11,13 +10,14 @@ const MainAnimatedCard = ({
   heightOfMainCard: number;
 }) => {
   const { current } = useAppSelector((state) => state.current);
+  const currentLocation = useAppSelector((state) => state.location);
   const { temperatureUnit } = useAppSelector((state) => state.settings);
   return (
     <Animated.View
       style={[
         { flexDirection: +heightOfMainCard < 400 ? "row" : "column" },
         { alignItems: "center", justifyContent: "center" },
-        { marginBottom: 10 },
+        { marginBottom: 10, position: "relative" },
       ]}
     >
       <Image
@@ -40,6 +40,19 @@ const MainAnimatedCard = ({
         </Text>
         <Text style={{ color: Colors.text.main, fontSize: 16 }}>
           {current?.condition.text}
+        </Text>
+        <Text
+          style={{
+            color: Colors.text.main,
+            fontSize: 12,
+            position: "absolute",
+            bottom: -25,
+            left: -150,
+            width: 300,
+          }}
+        >
+          {current?.last_updated.slice(-5)} in {currentLocation.name},{" "}
+          {currentLocation.country}
         </Text>
       </View>
     </Animated.View>
