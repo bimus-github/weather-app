f9539b620db4f8e3ab02814aaa678eb0f363917c
bimus-github <bimus2022@gmail.com>
2024-02-05 16:08:22 +0500

    fixing

--- components/search/SearchBar.tsx
+++ components/search/SearchBar.tsx
@@ -5,22 +5,50 @@ import {
   TextInput,
   Image,
   TouchableOpacity,
+  ActivityIndicator,
 } from "react-native";
 import React, { useState } from "react";
 import Colors from "@/constants/Colors";
 import { search } from "@/constants/Icons";
+import { Data_Type } from "@/models";
+import { getDataByName } from "@/hooks/getDataByName";
 
-const SearchBar = () => {
+const SearchBar = ({
+  result,
+  setResult,
+}: {
+  result: Data_Type | undefined;
+  setResult: (result: Data_Type) => void;
+}) => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const [city, setCity] = useState<string>("");
-  const handleSearch = () => {};
+
+  const handleSearch = () => {
+    if (city) {
+      setIsLoading(true);
+      getDataByName({ name: city })
+        .then((data) => {
+          if (data) {
+            setResult(data);
+          }
+        })
+        .finally(() => {
+          setIsLoading(false);
+        });
+    }
+  };
   return (
     <View style={styles.bar}>
-      <TouchableOpacity onPress={handleSearch}>
-        <Image
-          source={search}
-          style={{ width: 20, height: 20, tintColor: "#4f4f4f" }}
-        />
-      </TouchableOpacity>
+      {isLoading ? (
+        <ActivityIndicator color={Colors.text.secondary} />
+      ) : (
+        <TouchableOpacity onPress={handleSearch}>
+          <Image
+            source={search}
+            style={{ width: 20, height: 20, tintColor: "#4f4f4f" }}
+          />
+        </TouchableOpacity>
+      )}
       <TextInput
         style={{ flex: 1 }}
         placeholderTextColor={Colors.text.placeholder}
