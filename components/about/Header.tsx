0ab14e1898fa7cf21fca656cb7793c8f09ddd0e4
bimus-github <bimus2022@gmail.com>
2024-02-05 16:07:54 +0500

    fixing

---
+++ components/about/Header.tsx
@@ -0,0 +1,31 @@
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+import React from "react";
+import Colors from "@/constants/Colors";
+import { backArrow } from "@/constants/Icons";
+import { useRouter } from "expo-router";
+
+const Header = () => {
+  const router = useRouter();
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={() => router.back()}>
+        <Image source={backArrow} style={styles.headIcon} />
+      </TouchableOpacity>
+      <Text style={styles.title}>About</Text>
+      <View></View>
+    </View>
+  );
+};
+
+export default Header;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+  },
+  headIcon: { width: 32, height: 32, tintColor: Colors.text.main },
+  title: { color: Colors.text.main, fontWeight: "600", fontSize: 16 },
+});
